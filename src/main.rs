26b8a3b1737d5a760651ed790e8a2a5422a3a1f1
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-04-15 12:08:13 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::path::PathBuf;
-use regex::Regex;
 use chrono::{offset::TimeZone, Local, NaiveDateTime};
 use clap::{AppSettings, Clap};
+use regex::Regex;
+use std::path::PathBuf;
 
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
@@ -12,17 +12,16 @@ struct Opts {
     /// Input file path
     file: PathBuf,
     /// Bucket size
-     #[clap(short, long, default_value = "3600")]
-    bucket_size: u64
+    #[clap(short, long, default_value = "3600")]
+    bucket_size: u64,
 }
 
 fn main() -> std::io::Result<()> {
     let opts: Opts = Opts::parse();
-
     let re = Regex::new(r#"^: (\d+):"#).unwrap();
-
     let history = std::fs::read(&opts.file)?;
     let history = String::from_utf8_lossy(&history);
+
     for command in history.lines() {
         let capture = re.captures_iter(command).next();
         if capture.is_none() {
@@ -30,7 +29,9 @@ fn main() -> std::io::Result<()> {
         }
         let captured = &capture.unwrap()[1];
         let utc_date_time = NaiveDateTime::parse_from_str(captured, "%s");
-        if utc_date_time.is_err() {continue}
+        if utc_date_time.is_err() {
+            continue;
+        }
         let local_date_time = Local.from_local_datetime(&utc_date_time.unwrap()).unwrap();
         println!("{}", local_date_time);
     }
