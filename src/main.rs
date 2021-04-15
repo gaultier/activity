aa34d977634c76f9182245c6d2ec18216475873c
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-04-15 11:08:03 +0200

    Add CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use std::env;
+use std::path::PathBuf;
 use regex::Regex;
 use chrono::{offset::TimeZone, Local, NaiveDateTime};
+use clap::{AppSettings, Clap};
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Input file path
+    file: PathBuf,
+    /// Bucket size
+     #[clap(short, long, default_value = "3600")]
+    bucket_size: u64
+}
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let opts: Opts = Opts::parse();
 
-    let re = Regex::new(r#": (\d+):"#).unwrap();
+    let re = Regex::new(r#"^: (\d+):"#).unwrap();
 
-    let history = std::fs::read(&args[1])?;
+    let history = std::fs::read(&opts.file)?;
     let history = String::from_utf8_lossy(&history);
     for command in history.lines() {
         let capture = re.captures_iter(command).next();
